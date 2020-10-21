e3fb2e4b3b7d5ee046459b7ba9c19082b3874144
Wojciech Maziarz <whitem200@gmail.com>
2020-10-21 20:56:50 +0200

    nowy branch
    do testowania

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main(String[] args){
         System.out.println("Hello world");
-	System.out.println("Lol");
+        System.out.println("Lol");
+        System.out.println(2+2);
+        System.out.println("Lol");
     }
 }
